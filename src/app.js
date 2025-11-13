730a1fd91795d2570a5af9d6f09420bde315208b
Justdeony <dlopez516@hotmail.com>
2025-11-13 01:06:22 +0000

    Project done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronouns = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let nouns = ['jogger', 'racoon'];
+  let tld = ['.com']
+
+  for (const pronoun of pronouns) {
+    for (const adjective of adj) {
+      for (const noun of nouns) {
+        for (const extention of tld) {
+          console.log(`${pronoun}${adjective}${noun}${extention}`);
+        }
+      }
+    }
+  }
+
+
+console.log(extention)
+
+
+
+
+
+
+
 };
